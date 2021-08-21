c6b50dc7b6293cdf0a8369ec847468f99f38022b
Nikita Zavorotnyi <zavorotnyni@gmail.com>
2021-08-21 12:45:29 +0800

    Editing

--- MyBlog/Controllers/ContentController.cs
+++ MyBlog/Controllers/ContentController.cs
@@ -24,7 +24,7 @@ namespace MyBlog.Controllers
         {
             applicationDb.States.Remove(applicationDb.States.FirstOrDefault(x => x.Id == id));
             applicationDb.SaveChanges();
-            return RedirectToAction("Index", new RouteValueDictionary(new {Controller="Home", Action = "Index"}));
+            return RedirectToAction("Index", new RouteValueDictionary(new { Controller = "Home", Action = "Index" }));
         }
 
         [HttpGet]
@@ -36,6 +36,22 @@ namespace MyBlog.Controllers
         public IActionResult AddState(State state)
         {
             state.CreationDate = DateTime.Now;
+            applicationDb.States.Add(state);
+            applicationDb.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditState(int id)
+        {
+            return View(applicationDb.States.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult EditState(State state) 
+        {
+            var item = applicationDb.States.FirstOrDefault(x => x.Id == state.Id);
+            applicationDb.States.Remove(item);
             applicationDb.States.Add(state);
             applicationDb.SaveChanges();
             return View();
